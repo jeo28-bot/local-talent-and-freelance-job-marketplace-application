0abb290e93e19bc97edf7a5abf780b0e7f3910bf
Kobe Raf <jumaritokoberaf@gmail.com>
2025-08-20 15:18:30 +0800

    Naa nanay Login og Create user og naay i error I run lng ang composer, npm install og npm run dev. Palihog ko install sa Tailwind di ko kabalo hahaha. Naa sa resources/views/auth ang file sa login og create

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landingpage');
+    return view('auth.login');
 });
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
